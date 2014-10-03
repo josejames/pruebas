7b149bd74b61b521c5357ab149c60283f665d6f8
josejames <rodriguezjjaime@hotmail.com>
2014-10-03 01:00:14 -0500

    Calculator

---
+++ Calculator.py
@@ -0,0 +1,16 @@
+class Calculator():
+
+	def suma(self, num1, num2):
+		return num1+num2
+
+	def resta(self, num1, num2):
+		return num1-num2
+
+	def division(self, num1, num2):
+		return num1/num2
+
+	def multiplicacion(self, num1, num2):
+		return num1*num2
+
+#	def potencia(self, num1):
+#		return num1**
